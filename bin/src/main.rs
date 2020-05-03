131e3c683872b84078042db323767f5f19906d4f
Michael Budde <git@mbudde.dk>
2020-05-03 19:22:43 +0200

    Initial commit

---
+++ bin/src/main.rs
@@ -0,0 +1,46 @@
+use std::path::Path;
+
+use beancount_core as bc;
+use beancount_parser::parse;
+
+fn main() {
+    let filename = std::env::args().nth(1).expect("filename argument");
+    let line = std::env::args().nth(2).and_then(|s| s.parse().ok());
+
+    let id = std::cell::Cell::new(0);
+    print_transactions(Path::new(&filename), line, &id);
+}
+
+fn print_transactions(filename: &Path, tx_id: Option<usize>, id: &std::cell::Cell<usize>) {
+    let unparsed_file = std::fs::read_to_string(filename).expect(&format!("cannot read file {}", filename.display()));
+    let ledger = parse(&unparsed_file);
+    for dir in ledger.directives {
+        match dir {
+            bc::Directive::Transaction(tx) => {
+                if let Some(src) = tx.source {
+                    if let Some(tx_id) = tx_id {
+                        if id.get() == tx_id {
+                            print!("{}", src);
+                            std::process::exit(0);
+                        }
+                    } else {
+                        let desc = match tx.payee {
+                            Some(p) => format!("\"{}\" \"{}\"", p, tx.narration),
+                            None => format!("\"{}\"", tx.narration)
+                        };
+                        println!("{} {} * {}", id.get(), &tx.date, desc);
+                    }
+                }
+                id.set(id.get() + 1);
+            }
+            bc::Directive::Include(include_dir) => {
+                let mut path = Path::new(filename).to_path_buf();
+                path.pop();
+                path.push(&*include_dir.filename);
+                let path = path.canonicalize().expect("could not canonicalize path");
+                print_transactions(&path, tx_id, id);
+            }
+            _ => {}
+        }
+    }
+}
---
+++ vim/ftplugin/beancount.vim
@@ -0,0 +1,43 @@
+if exists("b:did_ftplugin") && b:did_ftplugin != 1
+    finish
+endif
+
+function! s:insert_transaction(result)
+    let insert_lnr = line('.')
+    let insert_parts = split(getline(insert_lnr), ' ')
+    let date = strftime("%Y-%m-%d")
+    if len(insert_parts) > 0
+        let date = date[:-len(insert_parts[0])-1] . insert_parts[0]
+    endif
+
+    let parts = split(a:result, ' ', 1)
+    if parts[0] ==# '-1'
+        let parts[1] = date
+        call setline(insert_lnr, join(parts[1:], ' '))
+    else
+        let tx = system('bean-copy-txn ' . expand('%:S') . ' ' . parts[0])
+        let lines = split(tx, "\n")
+        let first_line = substitute(lines[0], '^[^ ]\+', date, "")
+        call cursor(insert_lnr, 0)
+        call setline(insert_lnr, first_line)
+        call append(insert_lnr, lines[1:] + [""])
+        norm j$BB
+    end
+    normal f"l
+endfunction
+
+function! s:transaction_lines()
+    let lines = reverse(systemlist('bean-copy-txn ' . expand('%:S')))
+    call insert(lines, '-1 ' . strftime("%Y-%m-%d") . ' * ""')
+    return lines
+endfunction
+
+command! -nargs=0 CopyTransaction call fzf#run({
+            \ 'source': s:transaction_lines(),
+            \ 'sink': function("s:insert_transaction"),
+            \ 'options': ['--no-multi', '--with-nth=2..'],
+            \ 'down': '30%',
+            \ })
+
+inoremap <buffer> <C-t> <C-\><C-O>:CopyTransaction<CR>
+nnoremap <buffer> <C-t> :CopyTransaction<CR>
